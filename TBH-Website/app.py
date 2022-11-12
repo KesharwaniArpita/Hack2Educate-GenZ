29a4872c4933a39e02f7f3c1ecf76d5604862e80
rajharsh18 <rajharsh564@gmail.com>
2022-11-13 00:21:28 +0530

    - Removed API key - added images

--- TBH-Website/app.py
+++ TBH-Website/app.py
@@ -5,7 +5,7 @@ import re
 import string
 
 # api key
-api_key = 'AIzaSyDNIBOKOXIkHDWf8G2hMAuzNuAdc6TO6Ps'
+api_key = 'Enter Your API key here'
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 app = Flask(__name__)
